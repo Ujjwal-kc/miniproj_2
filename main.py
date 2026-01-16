149de8d57a49dfb01fe2efe2f853442a51d4ecec
Ujjwal KC <kcujwal98@gmail.com>
2026-01-16 21:46:49 +0545

    Saving in csv file

--- main.py
+++ main.py
@@ -19,19 +19,17 @@ def is_site_available(BASE_URL):
         return None
 
 
-response = is_site_available(BASE_URL)
-
 
 url_paths = []
 
 
-def get_paths(response):
+def pagination(response):
 
     soup = BeautifulSoup(response.content, 'html.parser')
     
 
     while True:
-        
+        yield soup
         next_btn = soup.find('li', class_='next')
 
         if not next_btn:
@@ -39,17 +37,34 @@ def get_paths(response):
 
             
         path = next_btn.find('a')['href']
-        url_paths.append(path)
 
         next_url = urljoin(response.url,path)
 
         response = requests.get(next_url)
         soup = BeautifulSoup(response.content, 'html.parser')
         
+        
 
 
-get_paths(response)
-print(url_paths)
+
+response = is_site_available(BASE_URL)
+
+with open('books.csv', 'w') as csvFile:
+
+    headers = ['Book Name','Price','Stock','Rating']
+
+    csvWriter = csv.writer(csvFile)
+
+    csvWriter.writerow(headers)
+
+    for page in pagination(response):
+        for product in page.find_all('article', class_='product_pod'):
+            title = product.find('img').get('alt')
+            price = product.find('p', class_='price_color').get_text()
+            stock = product.find('p', class_='instock').get_text().strip()
+            rating = product.find('p', class_='star-rating').get('class')[1]
+            csvWriter.writerow([title, price, stock, rating])        
+
 
 
 
